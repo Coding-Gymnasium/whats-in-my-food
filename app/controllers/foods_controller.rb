d5d7d8a03fb8a2160244e70ccc22bf733133b0e4
Nico Rithner <nicorithner@gmail.com>
2020-10-19 11:18:27 -0600

    Create API call to retrieve foods

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,5 +1,8 @@
 class FoodsController < ApplicationController
   def index
-    
+    search_params = params[:q]
+    response = Faraday.get( "https://api.nal.usda.gov/fdc/v1/foods/search?api_key=#{ENV['USDA_API']}&query=#{search_params}")
+    @results = JSON.parse(response.body, symbolize_names: true)
+    @foods = @results[:foods] #array. Each food item is a hash.
   end
 end
