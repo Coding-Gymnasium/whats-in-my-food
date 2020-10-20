9d801c3a5dddbb2e49fd7af04ab16dedaa57bf9c
Nico Rithner <nicorithner@gmail.com>
2020-10-19 19:37:57 -0600

    Install webmock to use with vcr

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -64,6 +64,7 @@ RSpec.configure do |config|
 end
 VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  config.hook_into :webmock
   config.filter_sensitive_data('USDA_API') {ENV['USDA_API']}
   config.default_cassette_options = { re_record_interval: 7.days }
   config.configure_rspec_metadata!
