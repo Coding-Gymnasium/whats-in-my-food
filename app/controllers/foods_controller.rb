f84407e0308ba3870eae775147ea9ff832e91e7b
Nico Rithner <nicorithner@gmail.com>
2020-10-19 19:41:13 -0600

    Refactor controller
    Moved API call to facade.
    First step of refactor to facade pattern

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,8 +1,7 @@
 class FoodsController < ApplicationController
   def index
-    search_params = params[:q]
-    response = Faraday.get( "https://api.nal.usda.gov/fdc/v1/foods/search?api_key=#{ENV['USDA_API']}&query=#{search_params}")
-    @results = JSON.parse(response.body, symbolize_names: true)
-    @foods = @results[:foods] #array. Each food item is a hash.
+    @response = FoodsFacade.search_foods(params[:q])
+    @results = @response[:totalHits]
+    @foods = @response[:foods].first(10)
   end
 end
