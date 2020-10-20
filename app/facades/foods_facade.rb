ae08e96aac1a908f8ecf91e6952bdcfe312bde06
Nico Rithner <nicorithner@gmail.com>
2020-10-19 20:16:35 -0600

    Refactor facade and service for facade pattern

--- app/facades/foods_facade.rb
+++ app/facades/foods_facade.rb
@@ -1,8 +1,5 @@
 class FoodsFacade
   def self.search_foods(query)
-    search_params = query
-    response = Faraday.get( "https://api.nal.usda.gov/fdc/v1/foods/search?api_key=#{ENV['USDA_API']}&query=#{search_params}")
-    data = JSON.parse(response.body, symbolize_names: true)
-    {totalHits: data[:totalHits], foods: data[:foods] }
+    FoodService.search_foods_api(query)
   end
 end
