5112fa6e2c8d7f29da5f4fb1d2939c501993d7b1
Nico Rithner <nicorithner@gmail.com>
2020-10-20 08:10:52 -0600

    Remove non required data exposure

--- app/poros/food.rb
+++ app/poros/food.rb
@@ -1,15 +1,9 @@
 class Food
-  attr_reader :fdcId, :description, :dataType, :gtinUpc, :publishedDate, :brandOwner, :ingredients, :foodNutrients, :allHighlightFields, :score
+  attr_reader :description, :gtinUpc, :brandOwner, :ingredients
   def initialize(data)
-    @fdcId = data[:fdcId],
-    @description = data[:description],
-    @dataType = data[:dataType],
-    @gtinUpc = data[:gtinUpc],
-    @publishedDate = data[:publishedDate],
-    @brandOwner = data[:brandOwner],
-    @ingredients = data[:ingredients],
-    @foodNutrients = data[:foodNutrients],
-    @allHighlightFields = data[:allHighlightFields],
-    @score = data[:score]
+    @description = data[:description]
+    @gtinUpc = data[:gtinUpc]
+    @brandOwner = data[:brandOwner]
+    @ingredients = data[:ingredients]
   end
 end
