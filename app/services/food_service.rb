4f3f1c4d3b447e02e6a7adfb7adc427dfc202aa1
Nico Rithner <nicorithner@gmail.com>
2020-10-20 08:10:06 -0600

    Update service to query for 10 foods only

--- app/services/food_service.rb
+++ app/services/food_service.rb
@@ -2,6 +2,6 @@ class FoodService
   def self.search_foods_api(query)
     response = Faraday.get( "https://api.nal.usda.gov/fdc/v1/foods/search?api_key=#{ENV['USDA_API']}&query=#{query}")
     data = JSON.parse(response.body, symbolize_names: true)
-    {totalHits: data[:totalHits], foods: data[:foods] }
+    {totalHits: data[:totalHits], foods: data[:foods].first(10) }
   end
 end
